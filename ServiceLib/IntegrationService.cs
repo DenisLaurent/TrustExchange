3a0869a8374f1c152e9d6c33d7f3a86b6a4c7f37
denislaurent <lavrenov.denis@gmail.com>
2016-12-11 13:00:04 +0500

    as

---
+++ ServiceLib/IntegrationService.cs
@@ -0,0 +1,36 @@
+﻿using ServiceLib.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLib.Contracts.IntegrationService;
+
+namespace ServiceLib
+{
+    public class IntegrationService : IIntegrationService
+    {
+        public void DealClosed(DealClosedContract request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DocClosed(DocClosedContract request)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// уведомление банка отправителя о том что докусент попал в БЧ, проставляем ему addr транзакции БЧ
+        /// </summary>
+        /// <param name="request"></param>
+        public void DocCreated(DocCreatedContract request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DocReceived(DocReceivedContract request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
